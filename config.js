4a90c481be4d10ae5af8ae0946821be2d2ecabdc
srspkibrat <162491883+srspkibrat@users.noreply.github.com>
2024-03-06 19:24:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    'token': '', // Your bot token, activate all bot intents in discord developer portal.
-    'clientid': '', // Your discord bot id.
-    'guildid': '', // Id of a private server to load the bot's slash commands.
-    'ownerid': [''], // Add discord ids to the array to access the commands.
-    'webhook': '' // Your discord webhook url.
+    'token': 'MTIxNDk4MjY2MTAzMDkzNjY1Nw.GSn7iO.f1va8uqu8YBcw8SaxAso7Bk5KjKqfSqJF6R6_o', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1214982661030936657', // Your discord bot id.
+    'guildid': '1214982304846581870', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['1187138819816960031'], // Add discord ids to the array to access the commands.
+    'webhook': 'https://discord.com/api/webhooks/1214982339784998942/k_j9vjwVWyv-nDMUYTJ34tgPDVwfMIUS-vR2vqrQX2vk1QThmsc9nhDJFJx42k85l9Lx' // Your discord webhook url.
 }
